a93a51dee6507425a0633eca6d11060f7c64ca2d
BorisP1234 <borisp1234@proton.me>
2025-08-11 16:26:12 +0200

    color and vanish progress

---
+++ internal/multiproxy/util/color.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"go.minekube.com/common/minecraft/color"
+	"go.minekube.com/common/minecraft/component"
+)
+
+var ColorLightGreen, _ = color.Hex("#2ec52e")
+
+var StyleColorLightGreen = component.Style{
+	Color: ColorLightGreen,
+}
+
+var ColorOrange, _ = color.Hex("#ff8c00")
+
+var StyleColorOrange = component.Style{
+	Color: ColorOrange,
+}
+
+var ColorRed, _ = color.Hex("#e6201c")
+
+var StyleColorRed = component.Style{
+	Color: ColorRed,
+}
--- internal/proxy/commands/vanish.go
+++ internal/proxy/commands/vanish.go
@@ -1,8 +1,8 @@
 package commands
 
 import (
+	"github.com/team-vesperis/vesperis-mp/internal/multiproxy/util"
 	"go.minekube.com/brigodier"
-	"go.minekube.com/common/minecraft/color"
 	"go.minekube.com/common/minecraft/component"
 	"go.minekube.com/gate/pkg/command"
 	"go.minekube.com/gate/pkg/edition/java/proxy"
@@ -10,6 +10,39 @@ import (
 
 func (cm *CommandManager) vanishCommand(name string) brigodier.LiteralNodeBuilder {
 	return brigodier.Literal(name).
+		Then(brigodier.Literal("get").Executes(command.Command(func(c *command.Context) error {
+			p, ok := c.Source.(proxy.Player)
+			if !ok {
+				c.SendMessage(ComponentOnlyPlayersSubCommand)
+				return ErrOnlyPlayersSubCommand
+			}
+
+			mp, err := cm.mpm.GetMultiPlayer(p.ID())
+			if err != nil {
+				c.SendMessage(&component.Text{
+					Content: "Could not get vanish.",
+					S: component.Style{
+						Color:      util.ColorRed,
+						HoverEvent: component.ShowText(&component.Text{Content: "Internal error: " + err.Error(), S: util.StyleColorRed}),
+					},
+				})
+				return err
+			}
+
+			if mp.IsVanished() {
+				c.SendMessage(&component.Text{
+					Content: "Vanish is active.",
+					S:       util.StyleColorLightGreen,
+				})
+				return nil
+			}
+
+			c.SendMessage(&component.Text{
+				Content: "Vanish is not active.",
+				S:       util.StyleColorLightGreen,
+			})
+			return nil
+		})).Then(brigodier.Argument("target", brigodier.SingleWord))).
 		Then(brigodier.Literal("set").
 			Then(brigodier.Literal("on").Executes(command.Command(func(c *command.Context) error {
 				p, ok := c.Source.(proxy.Player)
@@ -21,9 +54,10 @@ func (cm *CommandManager) vanishCommand(name string) brigodier.LiteralNodeBuilde
 				mp, err := cm.mpm.GetMultiPlayer(p.ID())
 				if err != nil {
 					c.SendMessage(&component.Text{
-						Content: "Could not turn on vanish: error getting multiplayer.",
+						Content: "Could not turn on vanish.",
 						S: component.Style{
-							Color: color.Red,
+							Color:      util.ColorRed,
+							HoverEvent: component.ShowText(&component.Text{Content: "Internal error: " + err.Error(), S: util.StyleColorRed}),
 						},
 					})
 					return err
@@ -32,17 +66,64 @@ func (cm *CommandManager) vanishCommand(name string) brigodier.LiteralNodeBuilde
 				if mp.IsVanished() {
 					c.SendMessage(&component.Text{
 						Content: "Vanish is already active.",
-						S:       component.Style{},
+						S:       util.StyleColorOrange,
 					})
 					return nil
 				}
 
 				err = mp.SetVanished(true, true)
 				if err != nil {
-
+					c.SendMessage(&component.Text{
+						Content: "Could not turn on vanish.",
+						S: component.Style{
+							Color:      util.ColorRed,
+							HoverEvent: component.ShowText(&component.Text{Content: "Internal error: " + err.Error(), S: util.StyleColorRed}),
+						},
+					})
+					return err
 				}
 
 				return nil
 			}))).
-			Then(brigodier.Literal("off")))
+			Then(brigodier.Literal("off").Executes(command.Command(func(c *command.Context) error {
+				p, ok := c.Source.(proxy.Player)
+				if !ok {
+					c.SendMessage(ComponentOnlyPlayersSubCommand)
+					return ErrOnlyPlayersSubCommand
+				}
+
+				mp, err := cm.mpm.GetMultiPlayer(p.ID())
+				if err != nil {
+					c.SendMessage(&component.Text{
+						Content: "Could not turn off vanish: error getting multiplayer.",
+						S: component.Style{
+							Color:      util.ColorRed,
+							HoverEvent: component.ShowText(&component.Text{Content: "Internal error: " + err.Error(), S: util.StyleColorRed}),
+						},
+					})
+					return err
+				}
+
+				if !mp.IsVanished() {
+					c.SendMessage(&component.Text{
+						Content: "Vanish is not active.",
+						S:       util.StyleColorOrange,
+					})
+					return nil
+				}
+
+				err = mp.SetVanished(false, true)
+				if err != nil {
+					c.SendMessage(&component.Text{
+						Content: "Could not turn off vanish: error getting multiplayer.",
+						S: component.Style{
+							Color:      util.ColorRed,
+							HoverEvent: component.ShowText(&component.Text{Content: "Internal error: " + err.Error(), S: util.StyleColorRed}),
+						},
+					})
+					return err
+				}
+
+				return nil
+			}))))
 }
